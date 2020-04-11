394f5a67703107737217523d9911019e9c21f1fc
Vinicius Rodrigues <vrf92@msn.com>
2020-04-11 13:58:40 -0300

    Envio arquivos Formulário - email

--- js/script.js
+++ js/script.js
@@ -18,3 +18,14 @@ if (window.SimpleAnime) {
     new SimpleAnime();
 
 }
+
+if (window.SimpleForm) {
+
+    new SimpleForm({
+        form: ".formphp", // pega o formulário desejado
+        button: "#enviar", // seletor do botão que envia o formulário
+        erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+    });
+
+}
